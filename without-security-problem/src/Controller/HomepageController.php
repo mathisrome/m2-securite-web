3fb0bc68e4bc87d6f5ffc3116429744712e9b6ab
Mathis ROME <mathis.rome@icloud.com>
2024-11-11 13:48:23 +0100

    feat(xss): Setup project without security problem

---
+++ without-security-problem/src/Controller/HomepageController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\Type\ArticleType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HtmlSanitizer\HtmlSanitizerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('', name: 'homepage')]
+    public function index(
+        Request $request,
+        EntityManagerInterface $em,
+        HtmlSanitizerInterface $sanitizer
+    ): Response
+    {
+        $article = $em->getRepository(Article::class)->findOneBy([]);
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $em->persist($article);
+            $em->flush();
+        }
+
+        return $this->render('homepage/index.html.twig', [
+            'article' => $article,
+            "form" => $form,
+        ]);
+    }
+}
